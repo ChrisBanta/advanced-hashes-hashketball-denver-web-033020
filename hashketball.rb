56acaaa19e9e77a3fc1b58cec0479d21b69fa7e6
Chris Banta <chrisbanta1@gmail.com>
2020-03-23 23:25:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -168,9 +168,8 @@ end
 def team_colors(team_name)
   game_hash.each do |place, team|
     if team[:team_name]  == team_name
-
+      return team[:colors]
     end
-return team[:colors]
   end
 end
 
